529abd22e21f49ac6dd9d4936322e6904598e878
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-25 18:34:06 +0100

    reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+    return [
+        {title: 'No Scrubs', duration: '505'},
+        {title: 'Song 1', duration: '4:05'},
+        {title: 'No 4', duration: '3:05'},
+        {title: 'No 5', duration: '2:05'},
+        {title: 'No 5', duration: '1:05'}
+    ];
+};
+
+const selectedSongReducer = ( selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+
+    return selectedSong;
+};
