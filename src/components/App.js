e1e351220175bed3e481d5032aa55526d1b58069
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-26 13:54:31 +0100

    working redux

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
                 <div className="column eight wide">
                     <SongList />
                 </div>
-                <div className="column eigh wide">
+                <div className="column eight wide">
                     <SongDetail />
                 </div>
             </div>
