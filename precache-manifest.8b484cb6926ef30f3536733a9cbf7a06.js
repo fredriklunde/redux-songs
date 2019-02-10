3dca781b9c7e504b5eeb6222405dfa0ede0f2392
fredriklunde <fredrik.lunde3@gmail.com>
2019-02-10 10:29:39 +0100

    Updates

---
+++ precache-manifest.8b484cb6926ef30f3536733a9cbf7a06.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4248dbe4a05ffdbbfef",
+    "url": "/redux-songs/static/js/main.b4248dbe.chunk.js"
+  },
+  {
+    "revision": "dc5578cc851d341041fe",
+    "url": "/redux-songs/static/js/1.dc5578cc.chunk.js"
+  },
+  {
+    "revision": "5ad7208bdd658bb39c20",
+    "url": "/redux-songs/static/js/runtime~main.5ad7208b.js"
+  },
+  {
+    "revision": "16b4d8f7c218a09a7bc7337b4af69176",
+    "url": "/redux-songs/index.html"
+  }
+];
